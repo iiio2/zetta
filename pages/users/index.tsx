23ade2810491766a71bd9cf0f15246e9b85cc4d3
Mr. <gomexxxx71@gmail.com>
2025-09-18 22:51:13 +0600

    chore: user list

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,5 +1,33 @@
+import useFetch from '@/hooks/useFetch'
+import { getUsers } from '@/utils/queryFn'
+
+export interface User {
+  id?: number
+  name: string
+  username: string
+  phone: string
+  email: string
+}
+
 const Users = () => {
-  return <h3>hello, users</h3>
+  const data = useFetch('users', getUsers)
+
+  if (data.isLoading) return <p>Loading...</p>
+
+  // @ts-expect-error ignore users property
+  const users = data.data!.users as User[]
+
+  return (
+    <>
+      {users.map((user) => (
+        <div className='w-full border-2 p-3' key={user.id}>
+          <h3 className='text-2xl'>{user.name}</h3>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default Users
--- utils/queryFn.ts
+++ utils/queryFn.ts
@@ -3,3 +3,9 @@ export async function getPosts() {
   const posts = await response.json()
   return posts
 }
+
+export async function getUsers() {
+  const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/users')
+  const users = await response.json()
+  return users
+}
